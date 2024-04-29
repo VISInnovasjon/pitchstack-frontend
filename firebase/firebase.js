ec8c727dcb2a7cf9f1db5becb3e6110aebcd5ed7
ChristianAlnes <165920390+ChristianAlnes@users.noreply.github.com>
2024-04-29 09:32:51 +0200

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1S3KSBB3dMbDHPkffRg3NuHu56myIZkY",
-  authDomain: "vispitchstack.firebaseapp.com",
-  projectId: "vispitchstack",
-  storageBucket: "vispitchstack.appspot.com",
-  messagingSenderId: "923420991694",
-  appId: "1:923420991694:web:030118cd099a19f7ef309f"
+  apiKey: "AIzaSyDmITtXtM-RlaeC8F_pjEab21DRIbaYTIA",
+  authDomain: "vispitchdeck.firebaseapp.com",
+  projectId: "vispitchdeck",
+  storageBucket: "vispitchdeck.appspot.com",
+  messagingSenderId: "621123914404",
+  appId: "1:621123914404:web:cee5ff98004b6936c3b431"
 };
 
 const app = initializeApp(firebaseConfig);
